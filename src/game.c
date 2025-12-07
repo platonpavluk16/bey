a873ca4e42862fca107858c029512d676b8b6ebb
Platon <platon12802014@gmail.com>
2025-12-07 18:39:01 +0200

    add score wins and losses

--- src/game.c
+++ src/game.c
@@ -2,41 +2,107 @@
 #include <stdlib.h>
 #include <windows.h>
 #include <time.h> 
+#include <conio.h> 
 
+#define GRID_HEIGHT 10
+#define GRID_WIDTH 20
+#define NUM_TREES 5
+#define MOVES_TO_WIN 6
 
-int xTree[5];
-int yTree[5];
+int xTree[NUM_TREES];
+int yTree[NUM_TREES];
 int x = 5, y = 5;
 int enemyX = 10, enemyY = 3;
 char enemySymbol = '%';
 char playerSymbol;
+int score_wins = 0;
+int score_losses = 0;
 
-void draw() {
-    system("cls");
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 20; j++) {
 
-if (i == y && j == x)
-printf("%c", playerSymbol);
-else if (i == enemyY && j == enemyX)
-printf("%c", enemySymbol);
-else if (
-(j == xTree[0] && i == yTree[0]) ||
-(j == xTree[1] && i == yTree[1]) ||
-(j == xTree[2] && i == yTree[2]) ||
-(j == xTree[3] && i == yTree[3]) ||
-(j == xTree[4] && i == yTree[4])
-            )
-printf("T");
-else
-printf(".");
+int prevX, prevY; 
+
+
+void loadScore();
+void writeScore();
+void draw(int currentMoves); 
+void moveEnemy();
+void spawnTrees();
+void gameOver();
+void victory();
+int isTreeAt(int checkX, int checkY);
+void resetGame();
+
+
+
+void loadScore() {
+    FILE *score = fopen("score.txt", "r");
+    if (score != NULL) {
+
+        if (fscanf(score, "Wins: %d, Losses: %d", &score_wins, &score_losses) != 2) {
+
+            score_wins = 0;
+            score_losses = 0;
         }
-printf("\n");
+        fclose(score);
+    } else {
+
+        score_wins = 0;
+        score_losses = 0;
     }
 }
 
 
+void writeScore() {
+
+    FILE *score = fopen("score.txt", "w"); 
+    if (score != NULL) {
+        fprintf(score, "Wins: %d, Losses: %d\n", score_wins, score_losses);
+        fclose(score);
+    } else {
+        printf("Помилка: Не вдалося зберегти рахунок.\n");
+    }
+}
+
+
+int isTreeAt(int checkX, int checkY) {
+    for (int i = 0; i < NUM_TREES; i++) {
+        if (checkX == xTree[i] && checkY == yTree[i]) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
+void draw(int currentMoves) { 
+
+    system("cls"); 
+    
+    printf("Гравець: %c | Ворог: %c | Ходи: %d / %d | Перемоги: %d | Поразки: %d\n", 
+           playerSymbol, enemySymbol, currentMoves, MOVES_TO_WIN, score_wins, score_losses);
+    printf("--------------------\n");
+
+    for (int i = 0; i < GRID_HEIGHT; i++) {
+        for (int j = 0; j < GRID_WIDTH; j++) {
+
+            if (i == y && j == x) {
+                printf("%c", playerSymbol);
+            } else if (i == enemyY && j == enemyX) {
+                printf("%c", enemySymbol);
+            } else if (isTreeAt(j, i)) {
+                printf("T");
+            } else {
+                printf(".");
+            }
+        }
+        printf("\n");
+    }
+    printf("--------------------\n");
+}
+
+
 void moveEnemy() {
+
     if (enemyX < x) enemyX++;
     else if (enemyX > x) enemyX--;
 
@@ -44,86 +110,143 @@ void moveEnemy() {
     else if (enemyY > y) enemyY--;
 }
 
+
 void spawnTrees() {
-    for (int i = 0; i < 5; i++) {
-        xTree[i] = rand() % 20;
-        yTree[i] = rand() % 10;
+    for (int i = 0; i < NUM_TREES; i++) {
+        int newX, newY;
+        do {
+            newX = rand() % GRID_WIDTH;
+            newY = rand() % GRID_HEIGHT;
+
+        } while ((newX == x && newY == y) || (newX == enemyX && newY == enemyY));
+        
+        xTree[i] = newX;
+        yTree[i] = newY;
     }
 }
 
+
+void resetGame() {
+    x = 5; 
+    y = 5;
+    enemyX = 10;
+    enemyY = 3;
+    spawnTrees();
+}
+
+
+
 void gameOver() {
-    printf(
-"  ██████   █████  ███    ███ ███████        ██████  ██    ██ ███████ ██████  \n"
-" ██       ██   ██ ████  ████ ██            ██    ██ ██    ██ ██      ██   ██ \n"
-" ██   ███ ███████ ██ ████ ██ █████         ██    ██ ██    ██ █████   ██████  \n"
-" ██    ██ ██   ██ ██  ██  ██ ██            ██    ██  ██  ██  ██      ██   ██ \n"
-"  ██████  ██   ██ ██      ██ ███████        ██████    ████   ███████ ██   ██ \n"
+    system("cls");
+printf(
+        " ╔═══════╗ ╔═══════╗ ╔═══╗   ╔═══╗ ╔═══════╗      ╔═══════╗ ╔═╗   ╔═╗ ╔═══════╗ ╔═══════╗ \n"
+        " ║ █ ═ ═ ║ ║ █ ═ █ ║ ║ █ █ ╗ ║ █ █ ║ ║ █ ═ ═ ║      ║ █ ═ ═ ║ ║ █ ╗ ║ █ ║ █ ═ ═ ║ ║ █ ═ █ ║ \n"
+        " ║ █ ╔═█ ║ ║ █ ═ ═ ║ ║ █ ═ █ ║ ║ █ ║ ║ █ █ █ ║      ║ █ ═ ═ ║ ║ █ ║ ║ █ ║ █ █ █ ║ ║ █ █ █ ║ \n"
+        " ║ █ ═ ═ ║ ║ █ ═ █ ║ ║ █ ═ ═ ╝ ║ █ ║ ║ █ ═ ═ ║      ║ █ ═ ═ ║ ║ █ ║ ║ █ ║ █ ═ ═ ║ ║ █ ═ █ ║ \n"
+        " ╚═══════╝ ╚═══════╝ ╚═╝     ╚═╝ ╚═══════╝      ╚═══════╝ ╚═╝   ╚═╝ ╚═══════╝ ╚═══════╝ \n"
     );
     Sleep(2000);
-    exit(0);
+    score_losses++;
+    printf("\nПоразки: %d\n", score_losses);
+    writeScore();
+    Sleep(2000);
+    resetGame();
 }
 
 void victory() {
+    system("cls");
     printf(
-"██╗   ██╗██╗ ██████╗ ████████╗ ██████╗ ██████╗ ██╗   ██╗\n"
-"██║   ██║██║██╔═══██╗╚══██╔══╝██╔═══██╗██╔══██╗╚██╗ ██╔╝\n"
-"██║   ██║██║██║                  ██║   ██║██████╔╝ ╚████╔╝ \n"
-"██║   ██║██║████║   ██║   ██║██╔═══╝   ╚██╔╝  \n"
-"╚██████╔╝██║╚██████╔╝   ██║   ╚██████╔╝██║        ██║   \n"
-" ╚═════╝ ╚═╝ ╚═════╝    ╚═╝    ╚═════╝ ╚═╝        ╚═╝   \n"
+        "██╗   ██╗██╗ ██████╗ ████████╗ ██████╗ ██████╗ ██╗   ██╗\n"
+        "██║   ██║██║██╔═══██╗╚══██╔══╝██╔═══██╗██╔══██╗╚██╗ ██╔╝\n"
+        "██║   ██║██║██║      ██║  ██║  ██║██████╔╝ ╚████╔╝ \n"
+        "██║   ██║██║██║██║  ██║ ██║██╔═══╝  ╚██╔╝ \n"
+        "╚██████╔╝██║╚██████╔╝  ██║  ╚██████╔╝██║        ██║   \n"
+        " ╚═════╝ ╚═╝ ╚═════╝  ╚═╝   ╚═════╝ ╚═╝        ╚═╝   \n"
     );
     Sleep(2000);
-    exit(0);
+    score_wins++;
+    printf("\nПеремоги: %d\n", score_wins);
+    writeScore();
+    Sleep(2000);
+    resetGame();
 }
+
 
 int main() {
-    SetConsoleOutputCP(CP_UTF8);
-    SetConsoleCP(CP_UTF8);
+
+    SetConsoleOutputCP(65001); 
+    SetConsoleCP(65001);
     srand(time(NULL));
-    printf("Set your player symbol: ");
+
+
+    loadScore();
+    printf("Поточний Рахунок: Перемоги: %d, Поразки: %d\n", score_wins, score_losses);
+    
+    printf("Встановіть символ гравця: ");
     scanf(" %c", &playerSymbol);
-    spawnTrees();
+    
 
-for (int i = 0; i < 5; i++) {
-    if (x == xTree[i] && y == yTree[i]) {
-        gameOver();
-    }
-}
-
-
-    int moves = 0; 
-    char c;
-;
+    resetGame(); 
 
     while (1) {
-        draw();
-        printf("Move (w a s d), q to quit: ");
-        scanf(" %c", &c);
+        int moves = 0; 
+        char c;
+        
+        printf("--- ГРА ПОЧАТА ---\n");
+        printf("Натисніть Enter для початку.\n");
+        while ((getchar()) != '\n'); 
 
-        if (c == 'a') x--;
-        else if (c == 'd') x++;
-        else if (c == 'w') y--;
-        else if (c == 's') y++;
-        else if (c == 'q') break;
+        while (1) {
+            draw(moves); 
+            printf("Ходи (w a s d), q для виходу: ");
+            
 
-        moves++; 
+            c = _getch(); 
+            
 
-        moveEnemy();
+            prevX = x;
+            prevY = y;
 
-        if (enemyX == x && enemyY == y) {
-            gameOver();
-        }
-
-        for (int i = 0; i < 5; i++) {
-            if (x == xTree[i] && y == yTree[i]) {
-                if (x == xTree[i]){x = xTree[i] - 1;}
-                if (y == yTree[i]){y = yTree[i] - 1;}
-
+            if (c == 'a') x--;
+            else if (c == 'd') x++;
+            else if (c == 'w') y--;
+            else if (c == 's') y++;
+            else if (c == 'q') {
+                printf("\nГра завершена користувачем.\n");
+                return 0; 
             }
-}
 
-        if (moves >= 20) {
-            victory();
+            if (x < 0 || x >= GRID_WIDTH || y < 0 || y >= GRID_HEIGHT) {
+                x = prevX; 
+                y = prevY; 
+            }
+            
+
+            if (isTreeAt(x, y)) {
+
+                x = prevX;
+                y = prevY;
+            } else {
+
+                moves++; 
+            }
+
+
+            if (moves >= MOVES_TO_WIN) {
+                victory();
+                break; 
+            }
+
+
+            moveEnemy();
+
+
+            if (enemyX == x && enemyY == y) {
+                gameOver();
+                break;
+            }
+
+            Sleep(100); 
         }
     }
 
